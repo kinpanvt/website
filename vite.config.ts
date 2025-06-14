50886497dc31d0554d71fe7713ecf2c9c41cc509
Rishav Chakravarty <rishavchakravarty18@gmail.com>
2025-06-14 15:10:11 -0700

    Remove gh-pages cache folder

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio-website/',
+  base: '/website/',
   plugins: [react()],
   build: {
     outDir: 'dist'
