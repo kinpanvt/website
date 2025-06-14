9c44702cdc4626225cc49b261834d0efcfca73a2
Kinjal Pandey <145226963+kinpanvt@users.noreply.github.com>
2025-06-14 16:45:47 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/website/',
+  base: '',
   plugins: [react()],
   build: {
     outDir: 'dist'
